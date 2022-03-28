afdbd5423d33a451f0b0180affc7654186b0a822
TringuG <richardnalepka@gmail.com>
2022-03-28 13:35:06 +0000

    Fixed buttons colors

--- src/auth-modal.js
+++ src/auth-modal.js
@@ -141,7 +141,7 @@ ${
       </button>
     </div>
     <div class="flex items-center w-full">
-      <button x-on:click="$store.authModal.goTo('profile')" type="button" class="flex items-center justify-center w-full px-6 py-3 text-sm font-medium text-white transition-colors duration-200 transform bg-neutral-800 border-2 border-neutral-800 rounded-md hover:bg-black hover:border-black">
+      <button x-on:click="$store.authModal.goTo('profile')" type="button" class="flex items-center justify-center w-full px-6 py-3 text-sm font-medium text-black transition-colors duration-200 transform bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100">
         <svg class="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
           <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
         </svg>
@@ -158,7 +158,7 @@ ${
       </button>
     </div>
     <div class="flex items-center w-full">
-      <button x-on:click="$store.authModal.goTo('profile')" type="button" class="flex items-center justify-center w-full px-6 py-3 text-sm font-medium text-white transition-colors duration-200 transform bg-[#555555] border-2 border-[#555555] rounded-md hover:bg-black hover:border-black">
+      <button x-on:click="$store.authModal.goTo('profile')" type="button" class="flex items-center justify-center w-full px-6 py-3 text-sm font-medium text-black transition-colors duration-200 transform bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100">
         <svg class="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
         <path d="M22 17.607c-.786 2.28-3.139 6.317-5.563 6.361-1.608.031-2.125-.953-3.963-.953-1.837 0-2.412.923-3.932.983-2.572.099-6.542-5.827-6.542-10.995 0-4.747 3.308-7.1 6.198-7.143 1.55-.028 3.014 1.045 3.959 1.045.949 0 2.727-1.29 4.596-1.101.782.033 2.979.315 4.389 2.377-3.741 2.442-3.158 7.549.858 9.426zm-5.222-17.607c-2.826.114-5.132 3.079-4.81 5.531 2.612.203 5.118-2.725 4.81-5.531z"/>
         </svg>
@@ -205,7 +205,7 @@ ${
       </button>
     </div>
     <div class="flex items-center w-full">
-      <button x-on:click="$store.authModal.goTo('profile')" type="button" class="flex items-center justify-center w-full px-6 py-3 text-sm font-medium text-white transition-colors duration-200 transform bg-[#898F9C] border-2 border-[#898F9C] rounded-md hover:bg-black hover:border-black">
+      <button x-on:click="$store.authModal.goTo('profile')" type="button" class="flex items-center justify-center w-full px-6 py-3 text-sm font-medium text-black transition-colors duration-200 transform bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100">
       <img src="https://cdn.usbrandcolors.com/images/logos/facebook-logo.svg" class="w-4 h-4 mx-2 fill-current"></img>
       <span class="mx-2 sm:inline">Sign in with Facebook</span>
       </button>
@@ -229,7 +229,7 @@ ${
 
   <div class="flex justify-center items-center space-x-4 pt-4">
     <div class="flex items-center w-full">
-      <button x-on:click="$store.authModal.goTo('profile')" type="button" class="flex items-center justify-center w-full px-6 py-3 text-sm font-medium text-white transition-colors duration-200 transform bg-[#737373] border-2 border-[#737373] rounded-md hover:bg-gray-100 hover:text-black">
+      <button x-on:click="$store.authModal.goTo('profile')" type="button" class="flex items-center justify-center w-full px-6 py-3 text-sm font-medium text-black transition-colors duration-200 transform bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100">
       <img src="https://cdn.cdnlogo.com/logos/m/95/microsoft.svg" class="w-4 h-4 mx-2 fill-current"></img>
       <span class="mx-2 sm:inline">Sign in with Microsoft</span>
       </button>
