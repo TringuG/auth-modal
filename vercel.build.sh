a91a995095435f57d0ea78611110302abde8cfa7
Matej Bačo <matejbaco2000@gmail.com>
2022-04-01 19:28:17 +0000

    Fix Vercel

--- vercel.build.sh
+++ vercel.build.sh
@@ -2,5 +2,5 @@
 
 cp dist/auth-modal.js examples/auth-modal.js
 mv examples/appwrite.html examples/index.html
-sed -i 's/<script src="\.\.\/src\/auth-modal\.js" defer><\/script>/<script src="auth-modal.js" defer><\/script>/g' examples/index.html
+sed -i 's/<script src="\.\.\/dist\/auth-modal\.js" defer><\/script>/<script src="auth-modal.js" defer><\/script>/g' examples/index.html
 sed -i 's/\/examples\/appwrite\.html/\//g' examples/index.html
